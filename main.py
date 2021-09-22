6573f8817ad86a48cbc7b823c36bc33d61687ebc
Kamil Mlody <32574552+PrivateUseOnly@users.noreply.github.com>
2021-09-22 19:35:04 +0200

    final commit

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ wait = input("ARE YOU READY? When ready press enter\n")
 action = input("The pilot: Where do you will to be dropped?\n"
                "type: E - to pick east side. N - north side or S - south side.\n"
                "After choosing press enter:\n")
-action.upper()
+action = action.upper()
 if action == "S":
     action = input(f"\nYou are near the Maintenance door, what you want to do?\n"
                    f"B for Breach in, H for Hide and wait, O for Open the door\n")
-    action.upper()
+    action = action.upper()
     if action == "H":
         print("\nThe worker came the doors are left open")
         action = input("What you want to do? \n"
@@ -51,35 +51,35 @@ if action == "S":
                        "D - Distract him with a stone\n"
                        "S - Sneak in\n")
 
-        action.upper()
+        action = action.upper()
         if action == "S":
             print("\nYou're in! You've looked around and you choose your path")
             action = input("C - open the ceiling and go along green pipes\n"
                            "F - open the floor and go along with black cables\n"
                            "R - you see a reactor sign pointing left, go this direction\n")
 
-            action.upper()
+            action = action.upper()
             if action == "C":
                 print("\nYou've been sneaking for about 5 min now, but you are in the crossing\n"
                       "Choose the way you want to go")
                 action = input("B - Go where the pipes are getting bigger\n"
                                "F - Go back to the Floor\n"
                                "S - Go where the pipes are getting smaller\n")
-                action.upper()
+                action = action.upper()
                 if action == "B":
                     print("\nYou are in the Reactor chamber. You plant the bomb,\n"
                           " but you need to set the time for the explosion")
                     action = input("10 - for 10 minutes\n"
                                    "2 - for 2 hours\n"
                                    "20 - for 20 seconds\n")
-                    action.upper()
+                    action = action.upper()
                     if action == "10":
                         print("\nNow you need to evacuate quickly...\n"
                               "whats the plan?")
                         action = input("C -Call command to pick you up while you sneaking out to randezvous point\n"
                                        "R - run to randezvous point and call command for pick up\n"
                                        "S - Call command to pick you up and sneak to randezvous point\n")
-                        action.upper()
+                        action = action.upper()
                         if action == "C":
                             print(f"\nMISSION ACCOMPLISHED!\n"
                                   f"CONGRATS {name}!")
